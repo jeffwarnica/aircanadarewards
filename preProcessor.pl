eb89375ec64789c6a0a303c794a102d6dbe2b57b
Jeff Warnica <jwarnica@redhat.com>
2017-04-24 09:16:36 -0400

    Better deal with partner AQS

--- preProcessor.pl
+++ preProcessor.pl
@@ -1,13 +1,36 @@
 #!/bin/perl
+use feature 'say';
 
 my @all_lines, @aqd_lines;
+my $debug = 0;
+
+my $last_flight;
 
 while(<>) {
 	next unless /^2017-/; 
+	my $isaqd = 1;
+	
+	
 	push @all_lines, $_;
+	chop;
+	say $_ if $debug;
+
 	($d,$l,$c,$awm,$aqd) = split(",");
-	next unless $aqd =~ /\d/; 
-	push @aqd_lines, $_;
+	$cur_flight = "$d$l";
+	
+
+	say "\taqd: $aqd" if $debug;
+	if ($cur_flight eq $last_flight) {
+		say "Matching flight. Bonus miles? Not a AQS" if $debug;
+		$isaqd = 0;
+	}
+
+	unless ($aqd =~ /\d/ || $aqd =~ /Not eligible/) {
+		say "AQD isn't a digit. or 'Not eligible', Not a AQS. [$aqd]" if $debug;
+		$isaqd = 0;
+	}
+	push @aqd_lines, "$_\n" if $isaqd;
+	$last_flight = $cur_flight;
 }
 
 @all_lines = sort  @all_lines;
